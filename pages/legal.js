34978762586ae9cc0d728522d8e19df5ee144f10
Davide Baldelli <davbaldelli@gmail.com>
2021-11-22 18:20:16 +0100

    first commit

---
+++ pages/legal.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+function Legal() {
+    let root = document.documentElement;
+
+    root.style.setProperty('--green', "#F1FAEE");
+
+    window.scrollTo(0, 0);
+
+    return (
+        <>
+            <div className="testolegale">
+                <div className="sub1">
+                    Non lavoriamo per i poteri forti
+                </div>
+                <div className="sub2">
+                    <p>Sfortunatamente ne Soros ne i Big farma ci finanziano e da questo progetto <strong>non ci
+                        guadagnamo un euro.</strong></p>
+                    <p>Da qualche settimana, mostrare il greenpass è diventato un obbligo e una cosa che facciamo
+                        quotidianamente al lavoro, ristorante, bar.</p>
+                    <p><strong>Facciamolo con stile allora!</strong></p>
+                    <p>Questo progetto serve per <strong>fare sorridere te e il povero lavoratore</strong> che passa ore
+                        a scannerizzare i greenpass fuori dagli ingressi con il cellulare.</p>
+                    <p>Sappiamo che non è tanto e il problema non si sconfigge così ma crediamo che <strong>nel nostro
+                        piccolo anche un sorriso possa fare la differenza </strong>(e poi siamo informatici, non
+                        sappiamo fare molto altro).</p>
+                </div>
+
+                <div className="sub3">
+                    E la privacy?
+                </div>
+                <div className="sub2">
+                    <p>Ottima domanda! <strong>Non salviamo niente!</strong></p>
+                    <p>Ma proprio niente niente dei tuoi dati personali.</p>
+                    <p>Ci siamo impegnati per far si che il processo di personalizzazione viene fatto nel tuo cellulare
+                        e tutte le informazioni restino li.</p>
+                    <p>Quando apri il sito è come se tu scaricassi una applicazione e al momento della salvataggio del
+                        greenpass personalizzato la disinstallassi.</p>
+                    <p>L’unica cosa che salviamo, un po come tutti i siti (ma in realtà di meno), sono dati
+                        sull’utilizzo (quanta gente ha provato a usare questo sito, ecc).</p>
+                    <p>Se non ti fidi comunque <strong>puoi contattare Samuele</strong> (samuele@nonstudio.org) per
+                        guardare il codice del progetto.</p>
+                </div>
+                <div className="sub3">
+                    La morale di tutto cio
+                </div>
+                <div className="sub2">
+                    <p><strong>Quindi è gratis e non mi rubate le informazioni?</strong></p>
+                    <p>Esatto, in generale fai bene a non fidarti (ed è anche il motivo per cui abbiamo scritto questa
+                        pagina lunghissima).</p>
+                    <p>Proprio per questo se il progetto ti piace e condividi i questi valori, <strong>puoi dare una
+                        mano anche tu! </strong></p>
+                    <p>Come?</p>
+                    <p><strong>Offrendo un caffè agli sviluppatori con una donazione.</strong> Non ci aspettiamo che il
+                        nostro tempo venga ripagato (non è il nostro obiettivo) ma è un gesto che a noi <strong>fa super
+                            piacere </strong>(anche perche i server sono da pagare)</p>
+                    <p>Oppure se come noi non hai un soldo, puoi <strong>condividere il progetto</strong> a qualcun
+                        altr* (cosi magari il caffe lo offre ad entrambi)</p>
+                </div>
+
+                <footer className="footer1">
+                    <button className="caffeButton">
+                        <Link to='/page1' className="caffe">Offrici un caffè</Link>
+                    </button>
+                    <div>
+                        <Link to='/page1' className="backHome">Torna alla home</Link>
+                    </div>
+                </footer>
+            </div>
+        </>
+    )
+}
+
+export default Legal;
---
+++ pages/page2.js
@@ -0,0 +1,144 @@
+import React, {useEffect, useRef, useState} from "react";
+import jsQR from "jsqr";
+import {get} from '@andreekeberg/imagedata'
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+import Link from "next/link";
+
+const qr = '/images/Qr.png';
+const camera = "/images/Camerasvg.svg";
+const image = "/images/Image.svg";
+const avanti = "/images/Avanti.svg";
+const fasi = "images/Group 5.svg";
+
+
+export default function Page2() {
+
+
+    //window.scrollTo(0, 0);
+
+    useEffect(() => {
+        const root = document.documentElement
+        root.style.setProperty('--green', "#FFFFFF");
+    })
+
+    const [show, setShow] = useState(false);
+
+    const [cameraIsVisible, setCameraVisibility] = useState(false);
+
+    const inputFile = useRef(null)
+
+    function onImageSelectorClick() {
+        inputFile.current.click();
+    }
+
+    function notifyQrCodeFound(code) {
+        //alert.success("ho trovato qr");
+        //setShow((s) => true)
+
+        console.log(code)
+    }
+
+    function notifyQrCodeNotFound() {
+        //alert.show("non ho trovato nessun qr");
+        //setShow((s) => false)
+        console.log("non ho trovato nessun qr")
+    }
+
+    function onImageInput(file) {
+        get(file, (error, imageData) => {
+            if (error) {
+                console.log(error)
+            } else {
+                let code = jsQR(imageData.data, imageData.width, imageData.height);
+                if (code) {
+                    notifyQrCodeFound(code.data)
+                } else {
+                    notifyQrCodeNotFound()
+                }
+            }
+        })
+    }
+
+
+    function handleTakePhoto(dataUri) {
+        get(dataUri, (error, imageData) => {
+            if (error) {
+                console.log(error)
+            } else {
+                let code = jsQR(imageData.data, imageData.width, imageData.height);
+                if (code) {
+                    notifyQrCodeFound(code.data)
+                } else {
+                    notifyQrCodeNotFound()
+                }
+            }
+        })
+        setCameraVisibility(false)
+    }
+
+
+    function onCameraInputClick() {
+        setCameraVisibility(true)
+    }
+
+    return (
+        <div>
+            <div className="fase">
+                <img src={fasi}/>
+            </div>
+            <div className="qrframe">
+                <div className="frame">
+                    <img src={qr} className="qr" alt={"qr icon"}/>
+                </div>
+            </div>
+            <div className="panel2">
+                <div className="guide">
+                    Carica il tuo greenpass
+                </div>
+                <div className="loadOptions">
+                    <div className="loadsx" onClick={onCameraInputClick}>
+                        <div onClick={() => setShow((s) => true)}>
+                            <img src={camera} className="icon" alt={"camera icon"}/>
+                        </div>
+                        <div className="loadLabel">
+                            Fotocamera
+                        </div>
+                    </div>
+                    <div className="loaddx" onClick={onImageSelectorClick}>
+                        <div onClick={() => setShow((s) => true)}>
+                            <img src={image} className="icon" alt={"gallery icon"}/>
+                        </div>
+                        <div className="loadLabel">
+                            Galleria
+                        </div>
+                        <input type="file" ref={inputFile} accept="image/png, image/jpeg"
+                               onChange={e => (onImageInput(e.target.files[0]))} style={{display: 'none'}}/>
+                    </div>
+                </div>
+                <div>
+                    {cameraIsVisible && <Camera
+                        onTakePhotoAnimationDone={(dataUri) => {
+                            handleTakePhoto(dataUri);
+                        }}
+                    />}
+                </div>
+                <div className="pagineOptions">
+                    <Link href="/page25">
+                        <div className="buttonAvanti2" style={{display: show ? "block" : "none"}}>
+                            <img src={avanti} className="avanti"/>
+                        </div>
+                    </Link>
+
+                    <Link href="/">
+                        <div className="buttonIndietro2" onClick={() => setShow((s) => false)}>
+                            <img src={avanti} className="indietro"/>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
